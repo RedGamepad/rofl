263f9d890a23eb5e5ba173108d8832d54001c347
RedGamepad <hexguards@gmail.com>
2017-09-24 15:21:23 +0300

    v0.1

---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stat : MonoBehaviour {
+    public Image cont;
+    private float currentHlth;
+    private float currentFill;
+
+    public float CurrentHlth
+    {
+        get
+        {
+            return currentHlth;
+        }
+
+        set
+        {
+            currentHlth = value;
+        }
+    }
+
+    public float CurrentFill
+    {
+        get
+        {
+            return currentFill;
+        }
+
+        set
+        {
+            currentFill = value;
+        }
+    }
+
+    public void DealDamage (float dmg)
+    {
+        CurrentHlth -= dmg;
+    }
+
+    // Use this for initialization
+    void Start () {
+        cont.GetComponent<Image>();
+        CurrentHlth = 100;
+        CurrentFill = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentFill = CurrentHlth / 100;
+        cont.fillAmount = CurrentFill;
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWeapon
+{
+    
+    void Effect();
+    void Damage(Stat hlth);
+}
+
+public class Weapon : IWeapon
+{
+    public Weapon(float dmg)
+    {
+        WDamage = dmg;
+    }
+
+    public float WDamage; // Урон оружия
+
+    public void Damage(Stat hlth)
+    {
+        hlth.DealDamage(WDamage);
+    }
+    public void Effect()
+    {
+
+    }
+}
